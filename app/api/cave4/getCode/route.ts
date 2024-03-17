4e19e5288f9e20af132789e8bb067348b154d820
BluezinfoSol <exhibitionismclemmi@gmail.com>
2024-03-17 10:35:38 +0530

    edge runtime

--- app/api/cave4/getCode/route.ts
+++ app/api/cave4/getCode/route.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const apiKey = searchParams.get("apiKey");
