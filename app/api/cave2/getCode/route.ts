498299245a1817854587541bf9bbc1a0ebfb2f18
GowthamS22 <gowtham@solution22.com.au>
2025-01-20 15:09:45 +1100

    change the secure connection

--- app/api/cave2/getCode/route.ts
+++ app/api/cave2/getCode/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const apiKey = searchParams.get("apiKey");
     const orderId = searchParams.get("orderId");
-    const response = await fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${apiKey}&action=getStatus&id=${orderId}`);
+    const response = await fetch(`https://api1.5sim.net/stubs/handler_api.php?api_key=${apiKey}&action=getStatus&id=${orderId}`);
     const data = await response.text();
     return NextResponse.json({ data: data, message: "success" });
 }
@@ -16,7 +16,7 @@ export async function POST(request: NextRequest) {
     const req = await request.json();
     try {
         const status   = req.status==='cancel' ? '-1' : '8' ;
-        const activate = await fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=setStatus&status=${status}&id=${req.orderId}`);
+        const activate = await fetch(`https://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=setStatus&status=${status}&id=${req.orderId}`);
         if (!activate.ok) {
             throw new Error(`Failed to set status: ${activate.statusText}`);
         }
--- app/api/cave2/route.ts
+++ app/api/cave2/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const apiKey = searchParams.get("api_key");
-    const response = await fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${apiKey}&action=getBalance`);
+    const response = await fetch(`https://api1.5sim.net/stubs/handler_api.php?api_key=${apiKey}&action=getBalance`);
     const data = await response.text();
     return NextResponse.json({ data: data, message: "success" });
 }
@@ -14,14 +14,14 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
     try {
         const req = await request.json();
-        const response = await fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=getNumber&service=${req.service}&operator=${req.operator}&country=${req.country}`);
+        const response = await fetch(`https://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=getNumber&service=${req.service}&operator=${req.operator}&country=${req.country}`);
         if (!response.ok) {
             throw new Error(`Failed to fetch data: ${response.statusText}`);
         }
         const data = await response.text();
         if (data !== "") {
             const accessnumber = data.split(':');
-            const activate = await fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=setStatus&status=1&id=${accessnumber[1]}`);
+            const activate = await fetch(`https://api1.5sim.net/stubs/handler_api.php?api_key=${req.apiKey}&action=setStatus&status=1&id=${accessnumber[1]}`);
             if (!activate.ok) {
                 throw new Error(`Failed to set status: ${activate.statusText}`);
             }
